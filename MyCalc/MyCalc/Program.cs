e968029e8144f963864e922a7eca17b18f50e2a0
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-25 20:31:08 +0100

    initial commit

---
+++ MyCalc/MyCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mycalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator v1.0.0");
+
+
+            //Getting first number
+            string? numberString1 = Console.ReadLine();
+
+            //Getting second number
+
+            string numberString2 = Console.ReadLine();  
+
+            //Getting operand
+
+
+            //
+
+
+        }
+    }
+}
+
