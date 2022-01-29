d1661be1e4daa78bc3f4805696f7b594109b16a3
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-29 11:22:47 +0100

    operandy

--- MyCalc/MyCalc/Program.cs
+++ MyCalc/MyCalc/Program.cs
@@ -11,17 +11,45 @@ namespace Mycalc
 
             //Getting first number
             string? numberString1 = Console.ReadLine();
+            float number1 = float.Parse(numberString1 ?? "0");
+
+
+
 
             //Getting second number
 
-            string numberString2 = Console.ReadLine();  
+            string? numberString2 = Console.ReadLine();
+            float number2 = float.Parse(numberString2 ?? "0");
 
             //Getting operand
 
+            
+            string? operand = Console.ReadLine();
 
+            //Calculation
+
+            float result = 0;
+            switch (operand)
+            {
+                case "+": 
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+
+            }
+
+           
             //Show result
 
-
+            Console.WriteLine(result);
 
         }
     }
