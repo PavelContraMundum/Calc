b302ac5643f604733d93ca40b12f44b05cf1a5cf
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-29 17:27:25 +0100

    metoda pro vložení čísla

--- MyCalc/MyCalc/Program.cs
+++ MyCalc/MyCalc/Program.cs
@@ -11,22 +11,21 @@ namespace Mycalc
 
             //Getting first number
             Console.WriteLine("Enter first number (float)");
-            var numberString1 = Console.ReadLine();
-            var isOkNumber1 = float.TryParse(numberString1, out var number1 );
-            if (!isOkNumber1)
+            var number1 = GetNumber();
+            if(number1 is null)
             {
                 Console.WriteLine("Wrong number. Good bye.");
-            }
-
+                return;
+             }
 
 
             //Getting second number
             Console.WriteLine("Enter second number (float)");
-            var numberString2 = Console.ReadLine();
-            var isOkNumber2 = float.TryParse(numberString2, out var number2);
-            if (!isOkNumber2)
+            var number2 = GetNumber();
+            if (number2 is null)
             {
                 Console.WriteLine("Wrong number. Good bye.");
+                return;
             }
 
             //Getting operand
@@ -40,20 +39,20 @@ namespace Mycalc
             switch (operand)
             {
                 case "+": 
-                    result = number1 + number2;
+                    result = number1.Value + number2.Value;
                     break;
                 case "-":
-                    result = number1 - number2;
+                    result = number1.Value - number2.Value;
                     break;
                 case "*":
-                    result = number1 * number2;
+                    result = number1.Value * number2.Value;
                     break;
                 case "/":
                     if(number2 == 0)
                     {
                         Console.WriteLine("Divide by zero. Error.");
                     }
-                    result = number1 / number2;
+                    result = number1.Value / number2.Value;
                     break;
                 default:
                     Console.WriteLine("Wrong operand. Good bye!");
@@ -65,6 +64,17 @@ namespace Mycalc
 
             Console.WriteLine(result);
 
+        }
+
+        private static float? GetNumber()
+        {
+            var numberString = Console.ReadLine();
+            var isOkNumber1 = float.TryParse(numberString, out var number);
+            if (!isOkNumber1)
+            {
+                return null;
+            }
+            return number;
         }
     }
 }
